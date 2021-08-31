10fd02a3f875c550019ea06dfa85f36937de7180
takashima <takashima@catallaxy.me>
2021-08-31 22:32:26 +0900

    引数、戻り値の利用とオプションによる条件分岐の方法を変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -6,370 +6,280 @@ require 'optparse'
 require 'etc'
 require 'date'
 
-params = ARGV.getopts('alr')
-
 def file_info
-  @all_file = []
+  all_file = []
   Dir.glob('*') do |item|
-    @all_file << item
-    @all_file.sort!
+    all_file << item
+    all_file.sort!
   end
+  all_file
 end
 
 def file_info_include_invisible_file
-  @all_file = []
+  all_file = []
   Dir.foreach('.') do |item|
-    @all_file << item
-    @all_file.sort!
+    all_file << item
+    all_file.sort!
   end
+  all_file
 end
 
-def ls_command_main
-  file_info
+def ls_command(option)
+  all_file =
+    if option[1] == 'a'
+      file_info_include_invisible_file
+    else
+      file_info
+    end
 
-  padding_all_file = @all_file.map { |file| file.ljust(17) }
-  line = @all_file.size / 4
+  padding_all_file = all_file.map { |file| file.ljust(17) }
+
+  padding_all_file.reverse! if option[0] == 'r'
+
+  line = all_file.size / 4
   file_arry = []
   padding_all_file.each_slice(line) do |a, b, c, d|
     file_arry << [a, b, c, d]
   end
+
   file_column = file_arry.transpose
   file_column.each do |x|
     puts x.join(' ')
   end
 end
 
-def r_option_main
-  file_info
+def l_option_main(option)
+  all_file =
+    if option[1] == 'a'
+      file_info_include_invisible_file
+    else
+      file_info
+    end
 
-  padding_all_file = @all_file.map { |file| file.ljust(17) }
-  reverse_arry = padding_all_file.reverse
+  all_file.reverse! if option[0] == 'r'
 
-  line = @all_file.size / 4
-  file_arry = []
-  reverse_arry.each_slice(line) do |a, b, c, d|
-    file_arry << [a, b, c, d]
+  block_arry = []
+  all_file.each do |file|
+    fs = File::Stat.new(file)
+    block_arry << fs.blocks
   end
+  block = block_arry.sum
+  puts "total #{block}"
+
+  file_arry = data_get(all_file)
+
   file_column = file_arry.transpose
   file_column.each do |x|
     puts x.join(' ')
   end
 end
 
-def a_option_main
-  file_info_include_invisible_file
-
-  padding_all_file = @all_file.map { |file| file.ljust(17) }
-
-  line = @all_file.size / 5
-  file_arry = []
-  padding_all_file.each_slice(line) do |a, b, c, d|
-    file_arry << [a, b, c, d]
-  end
-  file_column = file_arry.transpose
-  file_column.each do |x|
-    puts x.join(' ')
+def file_type(string_symbol)
+  case string_symbol[0..1]
+  when '04'
+    'd'
+  when '10'
+    '-'
+  else
+    '|'
   end
 end
 
-def l_option_main
-  file_info
-
-  block_arry = []
-  @all_file.each do |file|
-    fs = File::Stat.new(file)
-    block_arry << fs.blocks
-  end
-  block = block_arry.sum
-  puts "total #{block}"
-
-  data_get
-
-  detail_arry = @detail.transpose
-
-  detail_arry.each do |x|
-    puts x.join(' ')
-  end
-end
-
-def alr_option_main
-  file_info_include_invisible_file
-
-  @all_file.reverse!
-
-  block_arry = []
-  @all_file.each do |file|
-    fs = File::Stat.new(file)
-    block_arry << fs.blocks
-  end
-  block = block_arry.sum
-  puts "total #{block}"
-
-  data_get
-
-  detail_arry = @detail.transpose
-
-  detail_arry.each do |x|
-    puts x.join(' ')
-  end
-end
-
-def al_option_main
-  file_info_include_invisible_file
-
-  block_arry = []
-  @all_file.each do |file|
-    fs = File::Stat.new(file)
-    block_arry << fs.blocks
-  end
-  block = block_arry.sum
-  puts "total #{block}"
-
-  data_get
-
-  detail_arry = @detail.transpose
-
-  detail_arry.each do |x|
-    puts x.join(' ')
-  end
-end
-
-def ar_option_main
-  file_info_include_invisible_file
-
-  padding_all_file = @all_file.map { |file| file.ljust(17) }
-  reverse_arry = padding_all_file.reverse
-
-  line = @all_file.size / 4
-  file_arry = []
-  reverse_arry.each_slice(line) do |a, b, c, d|
-    file_arry << [a, b, c, d]
-  end
-  file_column = file_arry.transpose
-  file_column.each do |x|
-    puts x.join(' ')
-  end
-end
-
-def lr_option_main
-  file_info
-
-  @all_file.reverse!
-
-  block_arry = []
-  @all_file.each do |file|
-    fs = File::Stat.new(file)
-    block_arry << fs.blocks
-  end
-  block = block_arry.sum
-  puts "total #{block}"
-
-  data_get
-
-  detail_arry = @detail.transpose
-
-  detail_arry.each do |x|
-    puts x.join(' ')
-  end
-end
-
-def file_type
-  @a << case @string_symbol[0..1]
-        when '04'
-          'd'
-        when '10'
-          '-'
-        else
-          '|'
-        end
-end
-
-def special_permission
+def special_permission(string_symbol)
   special_permission_hash = {
     '1' => 'x',
     '2' => 'w',
     '4' => 'r'
   }
-  @a << special_permission_hash[@string_symbol[2]]
+  special_permission_hash[string_symbol[2]]
 end
 
-def owner_permission
-  @a << case @string_symbol[3]
-        when '0'
-          '---'
-        when '1'
-          '--x'
-        when '2'
-          '-w-'
-        when '3'
-          '-wx'
-        when '4'
-          'r--'
-        when '5'
-          'r-x'
-        when '6'
-          'rw-'
-        else
-          'rwx'
-        end
+def owner_permission(string_symbol)
+  case string_symbol[3]
+  when '0'
+    '---'
+  when '1'
+    '--x'
+  when '2'
+    '-w-'
+  when '3'
+    '-wx'
+  when '4'
+    'r--'
+  when '5'
+    'r-x'
+  when '6'
+    'rw-'
+  else
+    'rwx'
+  end
 end
 
-def group_permission
-  @a << case @string_symbol[4]
-        when '0'
-          '---'
-        when '1'
-          '--x'
-        when '2'
-          '-w-'
-        when '3'
-          '-wx'
-        when '4'
-          'r--'
-        when '5'
-          'r-x'
-        when '6'
-          'rw-'
-        else
-          'rwx'
-        end
+def group_permission(string_symbol)
+  case string_symbol[4]
+  when '0'
+    '---'
+  when '1'
+    '--x'
+  when '2'
+    '-w-'
+  when '3'
+    '-wx'
+  when '4'
+    'r--'
+  when '5'
+    'r-x'
+  when '6'
+    'rw-'
+  else
+    'rwx'
+  end
 end
 
-def other_user_permission
-  @a << case @string_symbol[5]
-        when '0'
-          '---'
-        when '1'
-          '--x'
-        when '2'
-          '-w-'
-        when '3'
-          '-wx'
-        when '4'
-          'r--'
-        when '5'
-          'r-x'
-        when '6'
-          'rw-'
-        else
-          'rwx'
-        end
+def other_user_permission(string_symbol)
+  case string_symbol[5]
+  when '0'
+    '---'
+  when '1'
+    '--x'
+  when '2'
+    '-w-'
+  when '3'
+    '-wx'
+  when '4'
+    'r--'
+  when '5'
+    'r-x'
+  when '6'
+    'rw-'
+  else
+    'rwx'
+  end
 end
 
-@permission = []
-@hard_link = []
-@owner = []
-@group = []
-@bytes = []
-@time = []
-@file_name = []
+def detail_data_get(all_file)
+  permission = []
+  hard_link = []
+  owner = []
+  group = []
+  bytes = []
+  time = []
+  file_name = []
 
-def detail_data_get
-  @all_file.each do |file|
+  all_file.each do |file|
     fs = File::Stat.new(file)
-    @permission << fs.mode
-    @hard_link << fs.nlink
-    @owner << fs.uid
-    @group << fs.gid
-    @bytes << fs.size
-    @time << fs.atime
-    @file_name << file
+    permission << fs.mode
+    hard_link << fs.nlink
+    owner << fs.uid
+    group << fs.gid
+    bytes << fs.size
+    time << fs.atime
+    file_name << file
   end
+  [permission, hard_link, owner, group, bytes, time, file_name]
 end
 
-def from_link_to_group_data
-  @b = []
-  @hard_link.each do |link|
-    @b << link.to_s.rjust(2, ' ')
+def from_link_to_group_data(hard_link, owner, group, permission_detail)
+  number_of_link = []
+  hard_link.each do |link|
+    number_of_link << link.to_s.rjust(2, ' ')
   end
-  @detail << @b
+  permission_detail << number_of_link
 
-  @c = []
-  @owner.each do |id|
-    @c << Etc.getpwuid(id).name.ljust(15, ' ')
+  owner_names = []
+  owner.each do |id|
+    owner_names << Etc.getpwuid(id).name.ljust(15, ' ')
   end
-  @detail << @c
+  permission_detail << owner_names
 
-  @d = []
-  @group.each do |id|
-    @d << Etc.getgrgid(id).name
+  group_names = []
+  group.each do |id|
+    group_names << Etc.getgrgid(id).name
   end
-  @detail << @d
+  permission_detail << group_names
 end
 
-def permission_data
-  @detail = []
-  @a = []
+def permission_data(permission)
+  permission_detail = []
+  permission_data_array = []
 
-  @permission.each do |symbol|
-    @string_symbol = symbol.to_s(8).rjust(6, '0')
-    file_type
-    special_permission
-    owner_permission
-    group_permission
-    other_user_permission
+  permission.each do |symbol|
+    string_symbol = symbol.to_s(8).rjust(6, '0')
+    permission_data_array << file_type(string_symbol)
+    permission_data_array << special_permission(string_symbol)
+    permission_data_array << owner_permission(string_symbol)
+    permission_data_array << group_permission(string_symbol)
+    permission_data_array << other_user_permission(string_symbol)
   end
 
-  @permission_arry = []
-  @a.each_slice(5) do |arry|
-    @permission_arry << arry.compact.join.ljust(12, ' ')
+  permission_arry = []
+  permission_data_array.each_slice(5) do |arry|
+    permission_arry << arry.compact.join.ljust(12, ' ')
   end
-  @detail << @permission_arry
+  permission_detail << permission_arry
 end
 
-def from_bytes_to_time_data
-  @e = []
-  @bytes.each do |byte|
-    @e << byte.to_s.rjust(4, ' ')
+def from_bytes_to_time_data(bytes, time, link_to_group_detail)
+  bytes_arry = []
+  bytes.each do |byte|
+    bytes_arry << byte.to_s.rjust(4, ' ')
   end
-  @detail << @e
+  link_to_group_detail << bytes_arry
 
-  @f = []
-  @time.each do |t|
+  time_array = []
+  time.each do |t|
     year = t.year
     month = t.month
     date = t.day
     file_maked_day = Date.new(year, month, date)
     half_years_ago = Date.today.prev_month(6)
 
-    @f << if file_maked_day <= half_years_ago
-            t.strftime('%-m %e %Y')
-          else
-            t.strftime('%-m %e %H:%M')
-          end
+    time_array << if file_maked_day <= half_years_ago
+                    t.strftime('%-m %e %Y')
+                  else
+                    t.strftime('%-m %e %H:%M')
+                  end
   end
-  @detail << @f
+  link_to_group_detail << time_array
 end
 
-def file_name_data
-  @g = []
-  @detail << @file_name.each do |name|
-    @g << name
+def file_name_data(file_name, bytes_to_time_detail)
+  file_names = []
+  bytes_to_time_detail << file_name.each do |name|
+    file_names << name
   end
+
+  bytes_to_time_detail
 end
 
-def data_get
-  detail_data_get
-  permission_data
-  from_link_to_group_data
-  from_bytes_to_time_data
-  file_name_data
+def data_get(all_file)
+  permission, hard_link, owner, group, bytes, time, file_name = detail_data_get(all_file)
+  permission_detail = permission_data(permission)
+  link_to_group_detail = from_link_to_group_data(hard_link, owner, group, permission_detail)
+  bytes_to_time_detail = from_bytes_to_time_data(bytes, time, link_to_group_detail)
+  file_name_data(file_name, bytes_to_time_detail)
 end
 
-if params['a'] && params['l'] && params['r']
-  alr_option_main
-elsif params['a'] && params['l']
-  al_option_main
-elsif params['a'] && params['r']
-  ar_option_main
-elsif params['l'] && params['r']
-  lr_option_main
-elsif params['l']
-  l_option_main
-elsif params['a']
-  a_option_main
-elsif params['r']
-  r_option_main
+params = ARGV.getopts('alr')
+
+option = []
+
+option <<
+  if params['r']
+    'r'
+  else
+    'nil'
+  end
+
+option <<
+  if params['a']
+    'a'
+  else
+    'nil'
+  end
+
+if params['l']
+  l_option_main(option)
 else
-  ls_command_main
+  ls_command(option)
 end
