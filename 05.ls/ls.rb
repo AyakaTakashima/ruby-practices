599dbadf70b8130ae4c0bdbc52de34188d86be8e
takashima <takashima@catallaxy.me>
2021-09-12 19:24:32 +0900

    データの考え方を縦方向から横方向に変更

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -6,10 +6,29 @@ require 'optparse'
 require 'etc'
 require 'date'
 
+def main
+  params = ARGV.getopts('alr')
+
+  files =
+    if params['a']
+      Dir.glob('*', File::FNM_DOTMATCH).sort
+    else
+      Dir.glob('*').sort
+    end
+
+  files.reverse! if params['r']
+
+  if params['l']
+    l_option_main(files)
+  else
+    ls_command(files)
+  end
+end
+
 COLUMN_COUNT = 3
 def calc_row_count(files)
   if files.size > COLUMN_COUNT
-    (files.size.to_f / COLUMN_COUNT).ceil
+    (files.size.to_f / COLUMN_COUNT).ceil if files.size > COLUMN_COUNT
   else
     1
   end
@@ -33,15 +52,14 @@ end
 
 def l_option_main(files)
   block = files.sum do |file|
-    fs = File::Stat.new(file)
-    fs.blocks
+    File::Stat.new(file).blocks
   end
   puts "total #{block}"
 
   file_table = get_data(files)
 
-  file_table.transpose.each do |row_files|
-    puts row_files.join(' ')
+  file_table.each do |row|
+    puts row.join(' ')
   end
 end
 
@@ -52,17 +70,8 @@ def file_type(permission_digit)
   when '10'
     '-'
   else
-    '|'
+    'l'
   end
-end
-
-def special_permission(permission_digit)
-  special_permission_hash = {
-    '1' => 'x',
-    '2' => 'w',
-    '4' => 'r'
-  }
-  special_permission_hash[permission_digit[2]]
 end
 
 def build_permission(char)
@@ -86,7 +95,7 @@ def build_permission(char)
   end
 end
 
-def buld_file_details(files)
+def build_file_details(files)
   files.map do |file|
     fs = File::Stat.new(file)
     file_detail = {}
@@ -101,101 +110,61 @@ def buld_file_details(files)
   end
 end
 
-def permission_data(file_details)
-  permission_data_array = []
+def get_permission_data(file_detail)
+  permission_digit = file_detail[:permissions].to_s(8).rjust(6, '0')
+  data_array = []
+  data_array << file_type(permission_digit)
+  data_array << build_permission(permission_digit[3])
+  data_array << build_permission(permission_digit[4])
+  data_array << build_permission(permission_digit[5])
 
-  file_details.each do |mode|
-    permission_digit = mode[:permissions].to_s(8).rjust(6, '0')
-    data_array = []
-    data_array << file_type(permission_digit)
-    data_array << special_permission(permission_digit)
-    data_array << build_permission(permission_digit[3])
-    data_array << build_permission(permission_digit[4])
-    data_array << build_permission(permission_digit[5])
-    permission_data_array << data_array
-  end
-  permission_array = []
-  permission_data_array.each do |data_array|
-    permission_array << "#{data_array.compact.join}  "
-  end
-
-  [permission_array]
+  data_array.join
 end
 
-def from_link_to_group_data(file_details, permission_details)
-  number_of_link = []
-  file_details.each do |number|
-    number_of_link << number[:hard_links].to_s.rjust(2, ' ')
-  end
-  permission_details << number_of_link
-
-  owner_names = []
-  file_details.each do |id|
-    owner_names << Etc.getpwuid(id[:owners]).name.ljust(15, ' ')
-  end
+def get_owner_and_group_names(file_detail)
+  permission_details = []
+  owner_names = Etc.getpwuid(file_detail[:owners]).name.ljust(15, ' ')
   permission_details << owner_names
 
-  group_names = []
-  file_details.each do |id|
-    group_names << Etc.getgrgid(id[:groups]).name
-  end
+  group_names = Etc.getgrgid(file_detail[:groups]).name
   permission_details << group_names
 end
 
-def from_bytes_to_time_data(file_details, link_to_group_details)
-  bytes_array = []
-  file_details.each do |number|
-    bytes_array << number[:bytes].to_s.rjust(4, ' ')
+def get_time_data(file_detail)
+  year = file_detail[:times].year
+  month = file_detail[:times].month
+  date = file_detail[:times].day
+  file_created_day = Date.new(year, month, date)
+  half_years_ago = Date.today.prev_month(6)
+
+  month = file_detail[:times].strftime('%-m').rjust(2, ' ')
+  date = file_detail[:times].strftime('%e').rjust(2, ' ')
+
+  if file_created_day <= half_years_ago
+    year = file_detail[:times].strftime('%Y').rjust(5, ' ')
+    [month, date, year]
+  else
+    time = file_detail[:times].strftime('%H:%M')
+    [month, date, time]
   end
-  link_to_group_details << bytes_array
-
-  time_array = []
-  file_details.each do |time|
-    year = time[:times].year
-    month = time[:times].month
-    date = time[:times].day
-    file_maked_day = Date.new(year, month, date)
-    half_years_ago = Date.today.prev_month(6)
-
-    time_array << if file_maked_day <= half_years_ago
-                    time[:times].strftime('%-m %e %Y')
-                  else
-                    time[:times].strftime('%-m %e %H:%M')
-                  end
-  end
-  link_to_group_details << time_array
-end
-
-def file_name_data(file_details, bytes_to_time_details)
-  file_names_array = []
-  file_details.each do |name|
-    file_names_array << name[:names]
-  end
-
-  bytes_to_time_details << file_names_array
 end
 
 def get_data(files)
-  file_details = buld_file_details(files)
-  permission_details = permission_data(file_details)
-  link_to_group_details = from_link_to_group_data(file_details, permission_details)
-  bytes_to_time_details = from_bytes_to_time_data(file_details, link_to_group_details)
-  file_name_data(file_details, bytes_to_time_details)
-end
+  file_details = build_file_details(files)
 
-params = ARGV.getopts('alr')
+  all_data = []
+  file_details.each do |file_detail|
+    row_data = []
+    row_data << get_permission_data(file_detail)
+    row_data << file_detail[:hard_links].to_s.rjust(3, ' ')
+    row_data << get_owner_and_group_names(file_detail)
+    row_data << file_detail[:bytes].to_s.rjust(4, ' ')
+    row_data << get_time_data(file_detail)
+    row_data << file_detail[:names]
 
-files =
-  if params['a']
-    Dir.glob('*', File::FNM_DOTMATCH).sort
-  else
-    Dir.glob('*').sort
+    all_data << row_data
   end
-
-files.reverse! if params['r']
-
-if params['l']
-  l_option_main(files)
-else
-  ls_command(files)
+  all_data
 end
+
+main
