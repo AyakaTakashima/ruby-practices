8f6ca62acb03c311ac0a33418f01dff43bc013c7
takashima <takashima@catallaxy.me>
2021-09-14 19:59:15 +0900

    メソッドを削除して定数にした＆年表示のpadding修正など

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -50,6 +50,7 @@ def build_file_info(files)
     row_data << file_detail[:size].to_s.rjust(4, ' ')
     row_data << build_time_info(file_detail)
     row_data << file_detail[:file_name]
+    row_data
   end
 end
 
@@ -70,44 +71,37 @@ end
 
 def build_permission_info(file_detail)
   permission_digit = file_detail[:mode].to_s(8).rjust(6, '0')
-  data_array = []
-  data_array << build_file_type_info(permission_digit)
-  data_array << build_permission(permission_digit[3])
-  data_array << build_permission(permission_digit[4])
-  data_array << build_permission(permission_digit[5])
-
-  data_array.join
+  [
+  FILE_TYPE_TABLE[permission_digit[0..1]],
+  PERMISSION_TABLE[permission_digit[3]],
+  PERMISSION_TABLE[permission_digit[4]],
+  PERMISSION_TABLE[permission_digit[5]]
+  ].join
 end
 
-def build_file_type_info(permission_digit)
-  file_type_info = {
-    '04' => 'd',
-    '10' => '-',
-    '12' => 'l'
-  }
-  file_type_info[permission_digit[0..1]]
-end
+FILE_TYPE_TABLE = {
+  '04' => 'd',
+  '10' => '-',
+  '12' => 'l'
+}
 
-def build_permission(char)
-  permission_info = {
-    '0' => '---',
-    '1' => '--x',
-    '2' => '-w-',
-    '3' => '-wx',
-    '4' => 'r--',
-    '5' => 'r-x',
-    '6' => 'rw-',
-    '7' => 'rwx',
-  }
-  permission_info[char]
-end
+PERMISSION_TABLE = {
+  '0' => '---',
+  '1' => '--x',
+  '2' => '-w-',
+  '3' => '-wx',
+  '4' => 'r--',
+  '5' => 'r-x',
+  '6' => 'rw-',
+  '7' => 'rwx',
+}
 
 def build_time_info(file_detail)
   file_creation_date = file_detail[:atime].to_date
   half_years_ago = Date.today.prev_month(6)
 
   if file_creation_date <= half_years_ago
-    file_detail[:atime].strftime('%_m %_e %Y ')
+    file_detail[:atime].strftime('%_m %_e  %Y')
   else
     file_detail[:atime].strftime('%_m %_e %H:%M')
   end
