3b39f19044a567115f16ca1569c8113a58a137cb
takashima <takashima@catallaxy.me>
2021-09-24 23:34:28 +0900

    メソッド間で何個かの引数を持ち運ぶスタイルから一つのハッシュで持ち運ぶ形に変更

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -5,13 +5,7 @@
 require 'optparse'
 
 def main
-  params = ARGV.getopts('l')
-  option =
-    if params['l']
-      'l'
-    else
-      'nil'
-    end
+  option = ARGV.getopts('l')
 
   if ARGV[0].nil?
     count_word(option)
@@ -21,105 +15,96 @@ def main
 end
 
 def count_word(option)
-  input = readlines
+  rows = readlines
   words_count_array = []
   bytes_count_array = []
-  results = []
 
-  input.each.with_index(1) do |each_row, i|
-    each_row.chomp!
-    words_count_array << each_row.split(' ').size
-    bytes_count_array << "#{each_row}\n".bytesize
-
-    word_count_info = {}
-    word_count_info[:number_of_rows] = i
-    word_count_info[:number_of_words] = words_count_array.sum
-    word_count_info[:bytes] = bytes_count_array.sum
-    results = build_result_array(word_count_info, option)
+  rows.each do |row|
+    row.chomp!
+    words_count_array << row.split(' ').size
+    bytes_count_array << "#{row}\n".bytesize
   end
 
-  puts results.join(' ')
-end
+  word_count_info = []
+  word_count_info << {
+    number_of_rows: rows.size,
+    number_of_words: words_count_array.sum,
+    bytes: bytes_count_array.sum
+  }
+  padded_result = create_padded_result(option, word_count_info)
 
-def build_result_array(word_count_info, option)
-  results = []
-  results << word_count_info[:number_of_rows].to_s.rjust(8)
-  if option == 'nil'
-    results << word_count_info[:number_of_words].to_s.rjust(7)
-    results << word_count_info[:bytes].to_s.rjust(7)
-  end
-  results
+  puts padded_result.join(' ')
 end
 
 def count_word_of_argv(option)
-  lines_array, words_array, bytes_array, item_array = build_array
+  count_info_list = build_count_info_list
 
-  if ARGV.size > 1
-    lines_array << lines_array.sum
-    if option == 'nil'
-      words_array << words_array.sum
-      bytes_array << bytes_array.sum
-    end
-    item_array << 'total'
+  total_number_of_words = 0
+  total_number_of_rows = 0
+  total_bytes = 0
+
+  count_info_list.map do |data_in_hash|
+    total_number_of_rows += data_in_hash[:number_of_rows]
+    total_number_of_words += data_in_hash[:number_of_words]
+    total_bytes += data_in_hash[:bytes]
   end
-  check_option = option
-  padded_result = create_padded_result(check_option, lines_array, words_array, bytes_array, item_array)
+  total_hash = {
+    number_of_rows: total_number_of_rows,
+    number_of_words: total_number_of_words,
+    bytes: total_bytes,
+    title: 'total'
+  }
 
-  result_array = padded_result.transpose
-  result_array.each do |array|
-    puts array.join(' ')
+  count_info_list << total_hash
+
+  padded_result = create_padded_result(option, count_info_list)
+  padded_result.each do |result_array|
+    puts result_array.join(' ')
   end
 end
 
-def build_array
-  lines_array = []
-  words_array = []
-  bytes_array = []
-  item_array = []
-
-  ARGV.each do |word|
-    if File.exist?(word)
-      Dir.glob(word) do |item|
-        count_info = count_file_info(item)
-        lines_array << count_info[:number_of_rows]
-        words_array << count_info[:number_of_words]
-        bytes_array << count_info[:bytes]
-        item_array << count_info[:file_name]
-      end
+def build_count_info_list
+  ret = []
+  ARGV.each do |file_path|
+    if File.exist?(file_path)
+      ret << count_file_info(file_path)
+      count_file_info(file_path)
     else
-      puts "wc: #{word}: open: No such file or directory"
+      puts "wc: #{file_path}: open: No such file or directory"
     end
   end
-  [lines_array, words_array, bytes_array, item_array]
+  ret
 end
 
 def count_file_info(file)
   if FileTest.directory?(file)
     puts "wc: #{file}: read: Is a directory"
   else
-    words = []
-    File.new(file).each do |each_row|
-      row_built_words = each_row.split(' ')
-      row_built_words.each { |word| words << word }
+    text = File.read(file)
+    words = text.each_line.flat_map do |row|
+      row.split(' ')
     end
 
     count_info = {}
     count_info[:number_of_words] = words.size
-    count_info[:number_of_rows] = File.open(file).read.count("\n")
+    count_info[:number_of_rows] = text.count("\n")
     count_info[:bytes] = FileTest.size(file)
-    count_info[:file_name] = file
+    count_info[:title] = file
   end
   count_info
 end
 
-def create_padded_result(check_option, lines_array, words_array, bytes_array, item_array)
-  result = []
-  result << lines_array.map { |file| file.to_s.rjust(8) }
-  unless check_option == 'l'
-    result << words_array.map { |file| file.to_s.rjust(7) }
-    result << bytes_array.map { |file| file.to_s.rjust(7) }
+def create_padded_result(option, count_info_list)
+  count_info_list.map do |count_info_hash|
+    each_file_count_info = []
+    each_file_count_info << count_info_hash[:number_of_rows].to_s.rjust(8)
+    unless option['l']
+      each_file_count_info << count_info_hash[:number_of_words].to_s.rjust(7)
+      each_file_count_info << count_info_hash[:bytes].to_s.rjust(7)
+    end
+    each_file_count_info << count_info_hash[:title] if ARGV
+    each_file_count_info
   end
-  result << item_array
 end
 
 main
