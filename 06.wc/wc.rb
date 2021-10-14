27c4a7c7cdafdce6aba59485dde05533ab24a421
takashima <takashima@catallaxy.me>
2021-10-14 20:01:03 +0900

    build_total_hashメソッドで作成されたtotal_hashを返すようにした

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -28,56 +28,42 @@ end
 
 def count_word_of_argv(option, file_paths)
   count_info_list = build_count_info_list(file_paths)
-
-  if file_paths.size > 1
-    build_total_hash(option, count_info_list)
-    padded_result = create_padded_result(option, count_info_list, file_paths)
-  else
-    padded_result = create_padded_result(option, count_info_list, file_paths) unless count_info_list == [nil]
-  end
-
+  count_info_list << build_total_hash(count_info_list) if file_paths.size > 1
+  padded_result = create_padded_result(option, count_info_list, file_paths)
   padded_result.each { |result_array| puts result_array.join(' ') }
 end
 
-def build_total_hash(option, count_info_list)
-  total_words_count = 0
-  total_rows_count = 0
-  total_bytes = 0
+def build_total_hash(count_info_list)
+  total_hash = {}
+  total_hash[:rows_count] = 0
+  total_hash[:words_count] = 0
+  total_hash[:bytes] = 0
 
   count_info_list.map do |data_in_hash|
-    total_rows_count += data_in_hash[:rows_count]
-    total_words_count += data_in_hash[:words_count] unless option['l']
-    total_bytes += data_in_hash[:bytes] unless option['l']
+    total_hash[:rows_count] += data_in_hash[:rows_count]
+    total_hash[:words_count] += data_in_hash[:words_count]
+    total_hash[:bytes] += data_in_hash[:bytes]
   end
-  total_hash = {}
-  total_hash[:rows_count] = total_rows_count
-  total_hash[:words_count] = total_words_count unless option['l']
-  total_hash[:bytes] = total_bytes unless option['l']
   total_hash[:file_name] = 'total'
 
-  count_info_list << total_hash
+  total_hash
 end
 
 def build_count_info_list(file_paths)
   ret = []
   file_paths.each do |file_path|
     if File.exist?(file_path)
-      ret << check_file_or_directory(file_path)
+      if FileTest.directory?(file_path)
+        puts "wc: #{file_path}: read: Is a directory"
+      else
+        text = File.read(file_path)
+        ret << count_file_info(file_path, text)
+      end
     else
       puts "wc: #{file_path}: open: No such file or directory"
     end
   end
   ret
-end
-
-def check_file_or_directory(file)
-  if FileTest.directory?(file)
-    puts "wc: #{file}: read: Is a directory"
-    []
-  else
-    text = File.read(file)
-    count_file_info(file, text)
-  end
 end
 
 def count_file_info(file, text)
@@ -100,15 +86,13 @@ end
 def create_padded_result(option, count_info_list, file_paths)
   count_info_list.map do |count_info|
     each_file_count_info = []
-    unless count_info == []
-      each_file_count_info << " #{format_value(count_info[:rows_count])}"
-      unless option['l']
-        each_file_count_info << format_value(count_info[:words_count])
-        each_file_count_info << format_value(count_info[:bytes])
-      end
-      each_file_count_info << count_info[:file_name] if file_paths
+    each_file_count_info << " #{format_value(count_info[:rows_count])}"
+    unless option['l']
+      each_file_count_info << format_value(count_info[:words_count])
+      each_file_count_info << format_value(count_info[:bytes])
     end
-    each_file_count_info
+    each_file_count_info << count_info[:file_name] if file_paths
+    each_file_count_info # .join
   end
 end
 
